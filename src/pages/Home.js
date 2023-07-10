46ee85acb906929018ad8907d21efaa7b0652eca
Luis Henrique Gomes Cardoso <luis.cardoso@dotzmkt.com.br>
2023-07-09 23:02:14 -0300

    feat(mainpage): adding MainPage Section
    welcome text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import MainPage from '../components/MainPage'
 
 const Home = () => {
   return( <div>
     <Navbar />
+    <MainPage />
   </div>
   );
 };
