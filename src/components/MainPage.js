0275941f3262fcba995779701a3faa96da7aaed7
Luis Henrique Gomes Cardoso <luis.cardoso@dotzmkt.com.br>
2023-09-12 14:34:11 -0300

    feat(mainpage): adding inicial page content and scroll animation

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,18 +1,25 @@
 import "./MainPageStyles.css"
 import React from "react"
+import Lottie from 'react-lottie';
+import * as animationData from './scrollanimation.json'
+import { ScrollButton } from "./Scroll";
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const MainPage = () => {
     return (
-        <div className="hero">
+        <div className="about">
             <section id="home">
                 <div className="hero-container">
                     <div className="hand-emoji"/>
-                    <h1 className="title">Hey, I'm <span className="title-highligth">Luis</span></h1>
+                    <h1 className="title">Olá, Eu sou o <span className="title-highligth">Luis</span>!</h1>
                 </div>
-                    <div className="hero-contant">
-                    <p className="hero-text">Reinvent your internal and external communication with swift and compelling content</p>
+                <div className="hero-contant">
+                    <p className="hero-text">Seja muito bem-vindo(a) ao meu portfólio. ✨</p>
                     <div className="hero-btn">
-                    <button className="btn">Projetos</button>
+                        <button className="btn">Projetos</button>
+                    </div>
+                    <div className="hero-contant-scroll">
+                        <ScrollButton />
                     </div>
                 </div>
             </section>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import MainPage from '../components/MainPage'
+import About from '../components/About'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const Home = () => {
   return( <div>
     <Navbar />
     <MainPage />
+    <About />
   </div>
   );
 };
